a4d0f096d124e60ec037b4cc0c619608ca5c6b60
Blimabru <bru.no@outlook.com.br>
2024-05-29 01:37:08 -0300

    Refatoração na comunicação entre o Arduino e o ESP para aumentar a agilidade e eliminar erros de sobreescrita de dados.

--- uart.ino
+++ uart.ino
@@ -1,41 +1,59 @@
-// Conecte o pino TX do ESP8266 ao pino RX1 (19) do Arduino Mega.
-// Conecte o pino RX do ESP8266 ao pino TX1 (18) do Arduino Mega.
-// Conecte os pinos GND de ambos os dispositivos.
-
-void enviaDadosESP(String dispositivo, String valor) {
-  
+void enviaDadosESP(String sensor, String valor) {
   ComunicacaoUART.flush();
-  ComunicacaoUART.println("Sensor: " + dispositivo + " - Valor: " + valor);
-
-  delay(150);
+  ComunicacaoUART.print("{\"sensor\":\"");
+  ComunicacaoUART.print(sensor);
+  ComunicacaoUART.print("\",\"valor\":\"");
+  ComunicacaoUART.print(valor);
+  ComunicacaoUART.println("\"}");
+  ComunicacaoUART.print("\n");
 }
 
-
 void recebeDadosESP() {
-  if (ComunicacaoUART.available() > 0) {                            // Verifica se há dados disponíveis para leitura
-    String dadosRecebidos = ComunicacaoUART.readStringUntil('\n');  // Lê a mensagem recebida
+  static char dadosRecebidos[200];
+  static unsigned int index = 0;
+  static bool mensagemCompleta = false;
 
-    Serial.print("Recebe ESP: ");
-    Serial.println(dadosRecebidos);
-    Serial.print("\n");
-    
-    int idxDispositivo = dadosRecebidos.indexOf("Dispositivo: "); // Localiza a posição da substring "Dispositivo: " na mensagem recebida
-    int idxEstado = dadosRecebidos.indexOf(" - Estado: ");  // Localiza a posição da substring " - Estado: " na mensagem recebida
+  while (ComunicacaoUART.available() > 0) {
+    char c = ComunicacaoUART.read();
 
-    // Verifica se ambas as substrings foram encontradas
-    if (idxDispositivo != -1 && idxEstado != -1) {
-      // Extrai o nome do dispositivo a partir da posição após "Dispositivo: "
-      String dispositivo = dadosRecebidos.substring(idxDispositivo + 13, idxEstado);
-
-      // Extrai o estado a partir da posição após " - Estado: "
-      String estado = dadosRecebidos.substring(idxEstado + 11);
-
-      dispositivo.trim(); // Apagar espaços em branco
-      estado.trim(); // Apagar espaços em branco
-
-      // Chama a função para processar e exibir o nome do dispositivo e seu estado
-      controleDispositivos(dispositivo, estado);
-      delay(150);
+    if (c == '{') {  // Início de mensagem
+      index = 0;
+      mensagemCompleta = false;
     }
+
+    if (!mensagemCompleta) {
+      dadosRecebidos[index++] = c;
+
+      if (c == '}') {                  // Fim de mensagem
+        dadosRecebidos[index] = '\0';  // Termina a string
+        mensagemCompleta = true;
+        processaDadosRecebidos(dadosRecebidos);
+        Serial.println(dadosRecebidos);
+        break;
+      }
+    }
+  }
+}
+
+void processaDadosRecebidos(char* dados) {
+  String dadosStr(dados);
+
+  Serial.println(dados);
+
+  // Encontra os índices do nome do dispositivo e do estado na mensagem
+  int idxInicioNome = dadosStr.indexOf("\"") + 1;
+  int idxFimNome = dadosStr.indexOf("\":", idxInicioNome);
+  int idxInicioValor = idxFimNome + 3;
+  int idxFimValor = dadosStr.indexOf("\"", idxInicioValor);
+
+  String operacao = dadosStr.substring(idxInicioNome, idxFimNome);
+  String valor = dadosStr.substring(idxInicioValor, idxFimValor);
+  bool enviado = "false";
+
+  // Passa os valores extraídos para a função controleDispositivos
+  if (operacao != "message") {
+    controleDispositivos(operacao.c_str(), valor.c_str());
+  } else {
+    Serial.println("Mensagem do sistema\n");
   }
 }
